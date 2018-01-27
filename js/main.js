f63facdd576fab1c4fcc594a69a38f668eff3fd4
James Newell <dcravenor@Jamess-MBP.home>
2018-01-27 20:36:29 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 console.log("Up and running!");
-
-var cardOne = "queen";
-
-var cardTwo = "queen";
-
-var cardThree = "king";
-
-var cardFour = "king";
-
+//Create an Array for cards
+var cards = ["queen", "queen", "king", "king"];
+//Create an Array for cardsInPlay
+var cardsInPlay = [];
+//Create variable cardOne
+var cardOne = cards[0];
+//Add first card to array
+cardsInPlay.push(cardOne);
+//Display user flipped card
 console.log("User flipped " + cardOne);
-
-console.log("User flipped " + cardThree);
+//Create varibale cardTwo
+var cardTwo = cards[2];
+//Add second card to array
+cardsInPlay.push(cardTwo);
+//Display user flipped card
+console.log("User flipped " + cardTwo);
+//if statement for cardsInPlay Array length
+if (cardsInPlay.length === 2) {
+//check for equality between cards within cardsInPlay array and provide user feedback
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+	  alert("Sorry, try again.");
+	}
+}
