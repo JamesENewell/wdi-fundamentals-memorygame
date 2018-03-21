41973858103d68a2fe4aa8f83936579ae65621c7
James Newell <dcravenor@Jamess-MBP.home>
2018-03-21 11:46:17 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,26 +1,33 @@
-console.log("Up and running!");
-//Create an Array for cards
-var cards = ["queen", "queen", "king", "king"];
-//Create an Array for cardsInPlay
+// Array for Cards
+var cards = ['queen', 'queen', 'king', 'king'];
+
+// Array to store cards in play
 var cardsInPlay = [];
-//Create variable cardOne
+
+// Variable cardOne for the first card the user flipped
 var cardOne = cards[0];
-//Add first card to array
+// Add card to array of cards in play
 cardsInPlay.push(cardOne);
-//Display user flipped card
+// Console.log() to show the card that the user flipped
 console.log("User flipped " + cardOne);
-//Create varibale cardTwo
+
+// Variable cardTwo to represent the second card the user flipped
 var cardTwo = cards[2];
-//Add second card to array
+// Add card to array of cards that are in play
 cardsInPlay.push(cardTwo);
-//Display user flipped card
+// Console.log() to show the card that the user flipped
 console.log("User flipped " + cardTwo);
-//if statement for cardsInPlay Array length
+
+
+// Check to see if two cards have been played
 if (cardsInPlay.length === 2) {
-//check for equality between cards within cardsInPlay array and provide user feedback
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-	  alert("Sorry, try again.");
-	}
+
+  // Check to see if two cards match and tell the feedback to user
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
 }
+
