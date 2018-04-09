9392819bac61137b4a0ceab861b4826d242f2925
James Newell <happy-james@hotmail.co.uk>
2018-04-09 10:10:48 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,33 +1,59 @@
-// Array for Cards
-var cards = ['queen', 'queen', 'king', 'king'];
+console.log("Up and running!");
 
-// Array to store cards in play
+var cards = [
+  { 
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+
 var cardsInPlay = [];
 
-// Variable cardOne for the first card the user flipped
-var cardOne = cards[0];
-// Add card to array of cards in play
-cardsInPlay.push(cardOne);
-// Console.log() to show the card that the user flipped
-console.log("User flipped " + cardOne);
+var checkForMatch = function () {
 
-// Variable cardTwo to represent the second card the user flipped
-var cardTwo = cards[2];
-// Add card to array of cards that are in play
-cardsInPlay.push(cardTwo);
-// Console.log() to show the card that the user flipped
-console.log("User flipped " + cardTwo);
-
-
-// Check to see if two cards have been played
-if (cardsInPlay.length === 2) {
-
-  // Check to see if two cards match and tell the feedback to user
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    console.log("You found a match!");
   } else {
-    alert("Sorry, try again.");
+    console.log("Sorry, try again.");
 
   }
-}
+};
 
+
+var flipCard = function (cardId) {
+
+  
+  console.log("User flipped " + cards[cardId].rank);
+
+  
+  cardsInPlay.push(cards[cardId].rank);
+
+  console.log(cards[cardId].cardImage)
+
+  console.log(cards[cardId].suit)
+
+  if (cardsInPlay.length === 2) {
+    
+    checkForMatch();
+  }
+};
+
+flipCard(0);
+
+flipCard(2);
